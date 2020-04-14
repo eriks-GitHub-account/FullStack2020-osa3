eb26e7e232215ea52d9899db9a376ac9a1acfb95
Erik <erik.peltola@metropolia.fi>
2020-04-14 18:29:59 +0300

    First

---
+++ puhelinluettelobackend/build/precache-manifest.d45b67c60586e34c637993005ee60069.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3613ff280f67d7c696372d2eee965fd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9572616f2e923827ad23",
+    "url": "/static/css/main.f697f85f.chunk.css"
+  },
+  {
+    "revision": "08d3aad3ecae6194104c",
+    "url": "/static/js/2.f518c3b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f518c3b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9572616f2e923827ad23",
+    "url": "/static/js/main.d6abd447.chunk.js"
+  },
+  {
+    "revision": "c940798f285500efd361",
+    "url": "/static/js/runtime-main.64749484.js"
+  }
+]);
